34a380fe43cad4bbd1c8ef001607e9f3aea521c3
Alessio Ragno <spideralessio97@gmail.com>
2019-02-22 18:11:27 +0100

    first commit

---
+++ autostart.sh
@@ -0,0 +1,23 @@
+
+
+#!/bin/bash
+xte 'key Return'
+xte 'usleep 100000'
+
+xte 'key Return'
+xte 'usleep 100000'
+
+
+#Entered Race
+xte 'key Up'
+xte 'usleep 100000'
+
+xte 'key Up'
+xte 'usleep 100000'
+
+xte 'key Return'
+xte 'usleep 100000'
+
+
+xte 'key Return'
+xte 'usleep 100000'
---
+++ autostart_quickrace.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+xte 'key Return'
+xte 'usleep 100000'
+
+xte 'key Return'
+xte 'usleep 100000'
+
+xte 'key Return'
+xte 'usleep 100000'
+
+xte 'key Down'
+xte 'usleep 100000'
+
+xte 'key Return'
+xte 'usleep 100000'
+
+xte 'key Return'
+xte 'usleep 100000'
+
+xte 'key Down'
+xte 'usleep 100000'
+xte 'key Down'
+xte 'usleep 100000'
+xte 'key Down'
+xte 'usleep 100000'
+xte 'key Down'
+xte 'usleep 100000'
+
+xte 'key Down'
+xte 'usleep 100000'
+xte 'key Down'
+xte 'usleep 100000'
+xte 'key Down'
+xte 'usleep 100000'
+
+xte 'str ---'
+
+xte 'key Return'
+xte 'usleep 100000'
+
+xte 'key Return'
+xte 'usleep 100000'
+
+xte 'key Up'
+xte 'usleep 100000'
+
+xte 'key Return'
+xte 'usleep 100000'
+xte 'key F2'
+xte 'usleep 100000'
+
+
+#Entered Race
+#xte 'key Up'
+#xte 'usleep 100000'
+
+#xte 'key Up'
+#xte 'usleep 100000'
+
+# xte 'key Return'
+# xte 'usleep 100000'
+
+# xte 'key Return'
+# xte 'usleep 100000'
+
+# xte 'key F2'
+# xte 'usleep 100000'
